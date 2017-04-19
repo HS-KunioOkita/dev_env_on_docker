b8e23757bac364b57bc8dae44586c71ea028747b
kunio okita <chocokunio@gmail.com>
2017-04-19 09:26:36 +0900

    added makedir script.

---
+++ make_dir.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+$DIRS="./gitlab/config ./gitlab/data ./gitlab/logs ./jenkins ./mongo/data/runtime/db ./mongo/dump ./svn/svn ./svn/backup ./svn/conf ./ldap/config ./ldap/data ./pgdata/ ./redmine/data"
+
+for d in $DIRS
+do
+    mkdir -p $DIRS
+done
