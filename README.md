f2a0bdfc7ea118c18aaa32c3f6dd73f7eff74c57
root <root@localhost.odaserver>
2017-09-19 11:06:45 +0900

    add concourse-ci

--- README.md
+++ README.md
@@ -22,9 +22,18 @@
 7. Portainer(Docker管理用GUI) 
     - port : 9000  
 
+8. ConcourseCI(パイプライン形式のCIツール)
+    - port : 8081
+
 # 手順
 
     $ git clone https://github.com/KunioOkita/dev_env_on_docker.git
     $ cd dev_env_on_docker
+    $ mkdir -p keys/web keys/worker
+    $ sudo ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+    $ sudo ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+    $ sudo ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+    $ cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+    $ cp ./keys/web/tsa_host_key.pub ./keys/worker
     $ chmod +x make_dir.sh && ./make_dir.sh
     $ docker-compose up
--- make_dir.sh
+++ make_dir.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-DIRS="./gitlab/config ./gitlab/data ./gitlab/logs ./jenkins ./mongo/data/runtime/db ./mongo/dump ./svn ./ldap/config ./ldap/data ./pgdata/ ./redmine/data"
+DIRS="./gitlab/config ./gitlab/data ./gitlab/logs ./jenkins ./mongo/data/runtime/db ./mongo/dump ./svn ./ldap/config ./ldap/data ./pgdata/ ./redmine/data ./keys/web ./keys/worker"
 
 for d in $DIRS
 do
     mkdir -p $DIRS
 done
+
+ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
+ssh-keygen -t rsa -f ./keys/web/session_signing_key -N ''
+ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
+
+cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
+cp ./keys/web/tsa_host_key.pub ./keys/worker
 
 chmod 0777 ./jenkins
