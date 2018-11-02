a0fde8b0ea7a4ef75b7cb0673496befbbe2ff74e
KunioOkita <chocokunio@gmail.com>
2018-11-02 22:19:26 +0900

    remove '"' only.

--- make_dir.sh
+++ make_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIRS="./gitlab/config ./gitlab/data ./gitlab/logs ./jenkins ./mongo/data/runtime/db ./mongo/dump ./svn ./ldap/config ./ldap/data ./pgdata/ ./redmine/data  ./redmine/files ./redmine/plugins ./redmine/themes""
+DIRS="./gitlab/config ./gitlab/data ./gitlab/logs ./jenkins ./mongo/data/runtime/db ./mongo/dump ./svn ./ldap/config ./ldap/data ./pgdata/ ./redmine/data  ./redmine/files ./redmine/plugins ./redmine/themes"
 
 for d in $DIRS
 do
